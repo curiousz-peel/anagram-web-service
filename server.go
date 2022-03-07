ff1d489ab79c6bca491c3feae6d675fa82fa0440
curiousz-peel <94068509+curiousz-peel@users.noreply.github.com>
2022-03-07 19:17:57 +0200

    Cleaned main server.go file

--- server.go
+++ server.go
@@ -3,35 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
+var WordsMap map[string][]string
+
 func anagramsHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!\n", request.URL.Path[1:])
-	// h := request.Header
-	// fmt.Fprintln(writer, h)
-
-	request.ParseForm()
-	fmt.Fprintln(writer, request.Form)
-	// word := request.Form.Get("word")
-	// fmt.Printf(word + "\n")
-
 	word := request.FormValue("word")
 	if word == "" {
-		fmt.Fprint(writer, "Inexistent or no value for word as URL parameter!")
+		fmt.Fprint(writer, "Inexistent or no value for 'word' as URL parameter!")
 		return
 	} else {
-		fmt.Printf(word + "\n")
+		ordered_word := orderWord(word)
+		fmt.Println(WordsMap[ordered_word])
 	}
 }
 func main() {
-	dir_path, err := os.Getwd()
-	if err != nil {
-		fmt.Printf(err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf(filepath.Join(dir_path, "words.txt") + "\n")
+
+	WordsMap = make(map[string][]string)
+	initWordsFromTXT(buildPathToTXT(), WordsMap)
+
 	http.HandleFunc("/anagrams", anagramsHandler)
 	http.ListenAndServe(":8080", nil)
 }
