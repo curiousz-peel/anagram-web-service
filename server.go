03de6d559240272ddac45e7a936698be5949c026
curiousz-peel <94068509+curiousz-peel@users.noreply.github.com>
2022-03-07 13:22:47 +0200

    Added word value extraction from URL with pattern http://localhost:8080/anagrams?word=your_word Added cross-platform way of building path to the words.txt

--- server.go
+++ server.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+func anagramsHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!\n", request.URL.Path[1:])
+	// h := request.Header
+	// fmt.Fprintln(writer, h)
+
+	request.ParseForm()
+	fmt.Fprintln(writer, request.Form)
+	// word := request.Form.Get("word")
+	// fmt.Printf(word + "\n")
+
+	word := request.FormValue("word")
+	if word == "" {
+		fmt.Fprint(writer, "Inexistent or no value for word as URL parameter!")
+		return
+	} else {
+		fmt.Printf(word + "\n")
+	}
 }
 func main() {
-	http.HandleFunc("/", handler)
+	dir_path, err := os.Getwd()
+	if err != nil {
+		fmt.Printf(err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf(filepath.Join(dir_path, "words.txt") + "\n")
+	http.HandleFunc("/anagrams", anagramsHandler)
 	http.ListenAndServe(":8080", nil)
 }
